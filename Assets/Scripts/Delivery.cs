434061b3ea1564c1198403f8ef104ea1c4f689f7
Chloe Pham <chloe.ph2906@gmail.com>
2025-06-23 11:26:08 +1000

    Use SpriteRender, Get component to change color of the driver

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(0, 0, 0, 0);
     [SerializeField] float destroyDelay = 0.2f;
     bool hasPackage;
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -16,12 +23,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
         }
         if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 
