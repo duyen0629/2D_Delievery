50199a8fda9fe25fe64f409e1431306c5cb2ca40
Chloe Pham <chloe.ph2906@gmail.com>
2025-06-23 10:56:50 +1000

    Add Package and Customer delivered

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Ouch!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Package")
+        {
+            Debug.Log("Package picked up");
+        }
+        if (other.tag == "Customer")
+        {
+            Debug.Log("Package delivered");
+        }
+    }
+
+}
